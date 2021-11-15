cf3dd0b4d1d81b71c66d4c8fc3e54ac179df0f57
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-15 13:57:15 -0700

    begin defining index page elements

---
+++ index.js
@@ -0,0 +1,15 @@
+// Provides Constants
+
+
+// host
+const port = 'http://localhost:3000'
+
+
+// SC Page Elements
+
+
+// Add default handle_submit
+// function handle_submit(e) {
+//   e.preventDefault()
+// }
+
