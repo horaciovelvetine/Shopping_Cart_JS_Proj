1ac970892cecf9dc0ab068e186a18b68ed91751e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-17 16:58:43 -0700

    add cliHandle, render, addToDom, and scaff HTML

--- src/sponItem.js
+++ src/sponItem.js
@@ -1,6 +1,7 @@
 class SponItem {
   static all = []
-  
+  static sponItemsList = document.getElementById('sponsoredItemsList')
+
   constructor(sItem) {
     // Sets up all of a sponsored_items attributes for display
     this.id = sItem.id
@@ -24,4 +25,53 @@ class SponItem {
     //Adds this spon item to the list of all the spon items
     SponItem.all.push(this);
   }
+  
+  addToDom(){
+    this.element.addEventListener('click', this.clickHandler)
+    SponItem.sponItemsList.appendChild(this.render())
+  }
+
+  render() {
+
+    // Need to find a way to make the button here add an Item to this cart w/o a page reload!!!
+    this.element.innerHTML = `
+      <li>
+        <div class="d-flex flex-row">
+          <div class="flex-column m-1">
+            <img class="shadow m-2 img-fluid" src="https://via.placeholder.com/115x115" data-lazy-type="image" data-src="https://via.placeholder.com/100x100">
+          </div>
+
+          <div class="col-8 lh-1 m-1">
+            <span>${this.name}</span><br>
+            <span>${this.rating}</span><br>
+            <span>${this.Price}</span><br>
+            <button type="button" class="btn btn-light btn-sm border shadow-sm w-100 py-1 my-2" id="addSponItemToCartBut" item-id="${this.id}">Add to cart</button>
+
+          </div>
+        </div>
+      </li>
+    `
+    return this.element
+  }
+
+  renderAdded() {
+    this.element.innerHTML = 
+      `
+      <li>
+        <div class="d-flex bg-light rounded align-items-center">
+          <div class="d-flex flex-fill justify-content-center w-100 ">
+            <p class="fs5 py-3 mt-3">Item added to cart Successfully!</p>
+          </div>
+        </div>
+      </li>
+    `
+    return this.element
+  }
+
+  clickHandler= (e) => {
+    //check what values e returns when clicked, add add/to/cart case, otherwise link back to the show item page, to grab the item id and redirect to the appropriate show page
+    // window.location = `${showItempath}/${this.id}`
+    debugger; 
+  }
+
 }
