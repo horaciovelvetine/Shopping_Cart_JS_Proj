11ddca0d047ed0df57c56c18f2afd64847b0d224
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-17 00:38:27 -0700

    pseudo code process for page, begin

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ const port = 'http://localhost:3000/api/v1'
 //   e.preventDefault()
 // }
 
+
+// On page load this page should check for and handle a few differnet things:
+// First: check for and instantiate any cartItem objects and append them to the page
+// Second: check for and instantiate any sponItem objects and append them to the page
+// Third: check for and instantiate any relItem objects and append them to the page
+
+//All of these elements should be present in the demo!!!
+// Process: Should fetch a cart
+
+cartApi.getCart()
