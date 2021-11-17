60f0f0f3c2720dbf1ff3d185dcf08e392d788d10
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-17 13:34:53 -0700

    removes code bloat, start fresh

--- src/cartItem.js
+++ src/cartItem.js
@@ -2,21 +2,5 @@ class cartItem{
   
   static all = []
   static parContainer = document.getElementById('cart-contents-list') 
+
 }
-
-
-//   constructor({name, rating, price, stock  }){
-//     // Sets attributes for the JSide copy of the style OBj 
-    
-//     //this.attr = attr
-
-
-//     //provides default template for each style list element
-//     this.element = document.createElement('li')
-//     this.element.dataset["id"] = id
-//     this.element.id = `style-${id}`
-
-
-//     this.element.addEventListener('click', this.handleStyleClick)
-//     Style.all.push(this)
-//   }
