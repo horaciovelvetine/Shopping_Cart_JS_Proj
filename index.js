8cf20d8661e0e4170912f86070cbf31cd37b0620
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-17 16:59:34 -0700

    show_item consts + add_to_cart button handle init

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-// Define host path
+// Define paths
 const port = 'http://localhost:3000/api/v1';
+const showItemPath = 'http://localhost:3000/items/'
 
 // Define specific Elements on the Page for ease of recall.
-const sponItemsList = document.getElementById('sponsoredItemsList')
-const cartItemsList = document.getElementById('cartItemsList')
+const addSponItemButton = docuement.getElementById('addSponItemToCartBut')
+
 
 // Define new instance call of JS Page Elements
 const cartApi = new CartApi(port);
 
 
+addSponItemButton.addEventListener('submit', addToCartHandler)
 
+function addToCartHandler(e) {
+  e.preventDefault()
+  
+  //Means creating some routes in the API for it to talk to the items in the backend!
+  itemApi.addItemToCart()
 
-
+  // Add a method to sponItem called renderAdded
+  e.target.renderAdded()
+}
 // Add default handle_submit
 // function handle_submit(e) {
 //   e.preventDefault()
