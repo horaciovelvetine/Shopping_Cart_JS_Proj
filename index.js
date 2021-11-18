7b4bcebbf6e45a052b9eeef610dd7257ccdd63b2
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-17 17:19:11 -0700

    adds new order_path const and button scaff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Define paths
 const port = 'http://localhost:3000/api/v1';
-const showItemPath = 'http://localhost:3000/items/'
+const showItemPath = 'http://localhost:3000/items/';
+const newOrderPath = 'http://localhost:3000/orders/new';
 
 // Define specific Elements on the Page for ease of recall.
 const addSponItemButton = docuement.getElementById('addSponItemToCartBut')
@@ -12,7 +13,10 @@ const cartApi = new CartApi(port);
 
 
 addSponItemButton.addEventListener('submit', addToCartHandler)
+newOrderButton.addEventListener('submit', newOrderHandler)
 
+
+//Will likely require being part of a form in order to pass in POST data when submitting the form itself. 
 function addToCartHandler(e) {
   e.preventDefault()
   
@@ -22,10 +26,11 @@ function addToCartHandler(e) {
   // Add a method to sponItem called renderAdded
   e.target.renderAdded()
 }
-// Add default handle_submit
-// function handle_submit(e) {
-//   e.preventDefault()
-// }
+
+// Redirects to the new order page, preferably with some data to help fill out the form
+function newOrderHandler(e) {
+  window.location = newOrderPath
+}
 
 
 // Fetches the cart from the API and begins parsing into relevant FE OBj copies for display.
