bbaf25b87bb0e4bd16b909c39b6ff8115dd918f4
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-19 12:21:47 -0700

    comment out for testing

--- src/relItem.js
+++ src/relItem.js
@@ -1,7 +1,7 @@
-class RelItem {
-  // has: id, name, category, rating, number_of_reviews, price(which may be a range so shouldbe a string here!)
-  static all = [];
-  static parContainer = document.GetElementById('relatedItemCarousel')
+// class RelItem {
+//   // has: id, name, category, rating, number_of_reviews, price(which may be a range so shouldbe a string here!)
+//   static all = [];
+//   static parContainer = document.GetElementById('relatedItemCarousel')
 
 
-}
+// }
