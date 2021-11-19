df6a31ff46a5d68a09846b15b8e448de154122d1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-19 12:21:39 -0700

    init attribute set constructor

--- src/cartItem.js
+++ src/cartItem.js
@@ -1,5 +1,21 @@
 class cartItem {
 
+//has id, name, rating, number_of_reviews price, quantity
+// CartItem = One of the item/style pairs the user has in thier cart
   static all = []
   static cartItemsList = document.getElementById('cartItemsList')
+
+  constructor({id, name, rating, number_of_reviews, price, quantity}){
+    // Sets the cartItems attributes 
+    this.id = id
+    this.name = name
+    this.rating = rating
+    this.number_of_reviews = number_of_reviews
+    this.price = price
+    this.quantity = quantity
+
+    //Set template HTML for ELEMENT
+    this.element = docuement.createElement('div')
+
+  }
 }
