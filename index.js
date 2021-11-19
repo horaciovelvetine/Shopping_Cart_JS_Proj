097bc196a3c1eae44d2b88f9d61e75e9d92002c0
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-19 12:20:14 -0700

    begin testing at top from step to step

--- index.js
+++ index.js
@@ -2,19 +2,23 @@
 const port = 'http://localhost:3000/api/v1';
 const showItemPath = 'http://localhost:3000/items/';
 const newOrderPath = 'http://localhost:3000/orders/new';
+// Define new instance call of JS Page Elements
+const cartApi = new CartApi(port);
+
+cartApi.getCartDetails();
+
+
 
 // Define specific Elements on the Page for ease of recall.
 // const addSponItemButton = docuement.getElementById('addSponItemToCartButton')
 // const newOrderButton = docuement.getElementById('newOrderButton')
+// const cartInfoDisplay = docuement.getElementById('cartInfoDisplay')
 
 
-// Define new instance call of JS Page Elements
-const cartApi = new CartApi(port);
 
 
 // Handles events listener from sponsored_items sidebar add?? (May need to be removed into the sponsored items class as a part of the setting up of each individual item)
-addSponItemButton.addEventListener('submit', addToCartHandler)
-//Will likely require being part of a form in order to pass in POST data when submitting the form itself. 
+// addSponItemButton.addEventListener('submit', addToCartHandler)
 function addToCartHandler(e) {
   e.preventDefault()
   debugger;
@@ -26,7 +30,7 @@ function addToCartHandler(e) {
 }
 
 //Should handle the Proceed to checkout button!
-newOrderButton.addEventListener('submit', newOrderHandler)
+// newOrderButton.addEventListener('submit', newOrderHandler)
 // Redirects to the new order page, preferably with some data to help fill out the form
 function newOrderHandler(e) {
   e.preventDefault
@@ -36,4 +40,3 @@ function newOrderHandler(e) {
 
 
 // Fetches the cart from the API and begins parsing into relevant FE OBj copies for display.
-cartApi.getCartDetails();
