61a3b77ae127ca52c4df1237fa7a55d7efe467f1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-19 14:07:02 -0700

    remvoes extra fslash

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Define paths
 const port = 'http://localhost:3000/api/v1';
-const showItemPath = 'http://localhost:3000/items/';
+const showItemPath = 'http://localhost:3000/items';
 const newOrderPath = 'http://localhost:3000/orders/new';
 // Define new instance call of JS Page Elements
 const cartApi = new CartApi(port);
