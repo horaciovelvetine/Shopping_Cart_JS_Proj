cd0ea47ff3a827e31ba5f48fd2732cb7ecc4d130
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-20 13:37:58 -0700

    removes all, for later

--- src/relItem.js
+++
@@ -1,7 +0,0 @@
-// class RelItem {
-//   // has: id, name, category, rating, number_of_reviews, price(which may be a range so shouldbe a string here!)
-//   static all = [];
-//   static parContainer = document.GetElementById('relatedItemCarousel')
-
-
-// }
