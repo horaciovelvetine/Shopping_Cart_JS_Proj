54c46fae28f1f9a5c2456498cdb9566be0ab337b
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-19 17:24:58 -0700

    fixes es6 syntax and mis-working node def

--- src/cart.js
+++ src/cart.js
@@ -4,25 +4,26 @@ class Cart {
   static belowCartInfoDisplay = document.getElementById('belowCartInfoDisplay')
   static sidebarCartInfoDisplay = document.getElementById('sidebarCartInfoDisplay')
   
-  constructor(data){
+  constructor({id, number_of_items, sub_total, items, styles, cart_items, related_items, sponsored_items}){
     
     // Begin setting values for a Cart OBj's attributes using the recieved data from the API
-    this.id = data.id
-    this.numberOfItems = data.attributes.number_of_items
-    this.subTotal = data.attributes.sub_total
+    //data.attributes
+    this.id = id
+    this.numberOfItems = `${number_of_items}`
+    this.subTotal = `${sub_total}`
 
     // Include nested attributes for objects which are passed thru to create the order...
-    this.items = data.attributes.items
-    this.styles = data.attributes.styles
-    this.cartItems = data.attributes.cart_items
-    this.relItems = data.attributes.related_items
-    this.sponItems = data.attributes.sponsored_items
+    this.items = items
+    this.styles = styles
+    this.cartItems = cart_items
+    this.relItems = related_items
+    this.sponItems = sponsored_items
 
 
     // Adds element attributes to cart
-    this.element = document.createElement('span')
-    this.element.dataset.id = this.id
-    this.element.id = `cart-${this.id}-info`
+    this.element = document.createElement('li')
+    this.element.dataset.id = id
+    this.element.id = `cart-${id}-info`
 
     //Updates the static variable for the current cart
     Cart.current.push(this)
@@ -30,10 +31,10 @@ class Cart {
   }
 
   attatchCartToDom() {
-    debugger
     
-    // Cart.belowCartInfoDisplay.appendChild(this.renderCartInfo())
-    // Cart.sidebarCartInfoDisplay.appendChild(this.renderCartInfo())
+    
+    Cart.belowCartInfoDisplay.appendChild(this.renderCartInfo())
+    Cart.sidebarCartInfoDisplay.appendChild(this.renderCartInfo())
   }
 
   renderCartInfo() {
@@ -41,6 +42,7 @@ class Cart {
     `
     Subtotal (${this.numberOfItems} items): <span class="fw-bolder"> $${this.subTotal}</span>
     `
+    return this.element
   }
   
 }
