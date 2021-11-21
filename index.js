d21f120af36b9bb1881b776b3eddd4afe449301e
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-21 13:13:42 -0700

    prettier

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // Define paths
-const port = 'http://localhost:3000/api/v1';
-const showItemPath = 'http://localhost:3000/items';
-const newOrderPath = 'http://localhost:3000/orders/new';
+const port = "http://localhost:3000/api/v1";
+const showItemPath = "http://localhost:3000/items";
+const newOrderPath = "http://localhost:3000/orders/new";
 // Define new instance call of JS Page Elements
 const cartApi = new CartApi(port);
 
-
-
-// !Gets all item related page elements & OO's the front end. 
+// !Gets all item related page elements & OO's the front end.
 cartApi.getCartDetails();
-
