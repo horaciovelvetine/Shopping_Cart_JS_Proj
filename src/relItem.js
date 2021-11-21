9eaeb05b9fdfe9fcd510c235466da1f007ff2634
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-21 13:13:35 -0700

    prettier

--- src/relItem.js
+++ src/relItem.js
@@ -2,24 +2,22 @@ class RelatedItem {
   static all = [];
   static relateItemList = document.querySelector("relatedItemsList");
 
-  // *id, name, category, rating, number_of_reviews, price 
-  constructor({id, name, category, rating, number_of_reviews, price}) {
-    
+  // *id, name, category, rating, number_of_reviews, price
+  constructor({ id, name, category, rating, number_of_reviews, price }) {
     // ?Sets all base attributes
-    this.id = `${id}`
-    this.name = `${name}`
-    this.category = `${category}`
-    this.rating = `${rating}`
-    this.number_of_reviews == `${number_of_reviews}`
-    this.price = `${price}`
+    this.id = `${id}`;
+    this.name = `${name}`;
+    this.category = `${category}`;
+    this.rating = `${rating}`;
+    this.number_of_reviews == `${number_of_reviews}`;
+    this.price = `${price}`;
 
     // ?Set elemented related attributes
-    this.element = document.createElement('li');
+    this.element = document.createElement("li");
     this.element.dataset.id = id;
-    this.element.id = `relatedItemNum-${this.id}`
+    this.element.id = `relatedItemNum-${this.id}`;
 
     // ?Lets the RelatedItem Class knos of its new frend.
-    RelatedItem.all.push(this)
-
+    RelatedItem.all.push(this);
   }
 }
