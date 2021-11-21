3400c4d023be8201a40ef74fc577fcc27ea13bd7
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-21 13:13:27 -0700

    prettier, and add back in relItem call

--- src/cartApi.js
+++ src/cartApi.js
@@ -1,48 +1,47 @@
 class CartApi {
   // Each instance of the CartApi makes a fetch to the server to get Cart info, which this class formats and sends to each relevant FE OBj method(s) for persitence.
-  
-  constructor(port){
+
+  constructor(port) {
     this.refUrl = `${port}/show_cart`;
   }
 
-
-    // json["data"]["attributes"]["sponsored_items"].forEach(i => {
-      //   const sponItem = new sponItem(i)
-      //   sponItem.addToDom()
-    // })
+  // json["data"]["attributes"]["sponsored_items"].forEach(i => {
+  //   const sponItem = new sponItem(i)
+  //   sponItem.addToDom()
+  // })
   getCartDetails() {
     fetch(this.refUrl)
-    .then(r => r.json())
-    .then( json => {
-      const cart = new Cart({id: json["data"].id, ...json["data"].attributes})
-      
-      // debugger
-      // ?Creates CartItems OBj
-      json["data"].attributes.cart_items.forEach((item) => {
-        const c = new CartItem(item);
-        c.attachToCart();
+      .then((r) => r.json())
+      .then((json) => {
+        const cart = new Cart({
+          id: json["data"].id,
+          ...json["data"].attributes,
+        });
+
+        // debugger
+        // ?Creates CartItems OBj
+        json["data"].attributes.cart_items.forEach((item) => {
+          const c = new CartItem(item);
+          c.attachToCart();
+        });
+
+        // ?Creates SponItems OBj
+        json["data"].attributes.sponsored_items.forEach((item) => {
+          const s = new SponsorItem(item);
+          s.attatchToDom();
+        });
+
+        // ?Create RelItems OBj
+        json["data"].attributes.related_items.forEach((item) => {
+          const r = new RelatedItem(item);
+          r.attatchToDom();
+        })
+
+        return cart;
       });
-
-      // ?Creates SponItems OBj
-      json["data"].attributes.sponsored_items.forEach((item) => {
-        const s = new SponsorItem(item);
-        s.attatchToDom();
-      });
-
-      // ?Create RelItems OBj
-      // json["data"].attributes.related_items.forEach((item) => {
-      //   const r = new RelatedItem(item);
-      //   r.attatchToDom();
-      // })
-
-      return cart;
-    });
   }
 
   sendUpdatePatch() {
     // TODO: PERSISTS THE CHANGES ON THE FRONT END OF THE CART TO THE BACK END AND GRABS THE NEWLY UDATED CART INFO
   }
-
 }
-
-
