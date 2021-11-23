c3a8fc35a7839ebedf36d1cb1df49a46959be9b1
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-22 17:26:19 -0700

    cartItem update event handled, prettier

--- src/cartItem.js
+++ src/cartItem.js
@@ -4,24 +4,15 @@ class CartItem {
   static all = [];
   static cartItemsList = document.getElementById("cartItemsList");
 
-  constructor({
-    id,
-    name,
-    rating,
-    number_of_reviews,
-    price,
-    quantity,
-    color,
-    manufacturer,
-  }) {
+  constructor({ id, item_name, style_name, rating, number_of_reviews, price, quantity, manufacturer }) {
     // Sets the cartItems attributes
     this.id = id;
-    this.name = `${name}`;
+    this.item_name = `${item_name}`;
+    this.style_name = `${style_name}`;
     this.rating = `${rating}`;
     this.number_of_reviews = `${number_of_reviews}`;
     this.price = `${price}`;
     this.quantity = `${quantity}`;
-    this.color = `${color}`;
     this.manufacturer = `${manufacturer}`;
 
     this.showItemPath = `${showItemPath}/${this.id}`;
@@ -35,11 +26,56 @@ class CartItem {
     this.element.addEventListener("click", this.handleCartItemClick);
     CartItem.all.push(this);
   }
+  updateCartItem(quantity) {
+    this.quantity = quantity;
+    this.renderCartItem()
+  }
+
+  handleCartItemClick = (e) => {
+    e.preventDefault();
+    // Provides a path and action for all of the cartItem elements on the page. This is the main hub for all CRUD Action Initiation.
+    let clickedOn = e.target.className;
+
+    switch (clickedOn) {
+      case "cartItemTitle":
+      case "cartItemPrice":
+      case "cartItemEligible":
+      case "cartItemStars":
+      case "cartItemColor":
+      case "cartItemManufacturer":
+        //should flash a notice on screen that careful, youre headed out of the one page app!
+        alert(
+          "Watch Out! You're about to head on your way out of my single page app, why don't ya turn on around!"
+        );
+        break;
+
+      case "dropdown-item":
+        //*updates the cartItem on the front end and DOM
+        this.updateCartItem(e.target.innerText)
+
+        //*updates the Cart FrontEnd and BackEnd wise, calls CartAPI for the update.
+        Cart.currentCart().updateCartInfo()
+        // ??Then similarly do the same with the cart, but in this case the loop also sends the persist update to the backend
+
+        break;
+
+      case "deleteItemFromCartLinkAndAddToList":
+        debugger;
+        break;
+
+      case "deleteItemFromCartLink":
+        debugger;
+        break;
+      default:
+        // ?acts as a catch for everything that got missed, and allows bstrap Popper JS to still handle its events correctly.
+        console.log("null event");
+        break;
+    }
+  };
   // Calls the render to change the inner text of whatever cartItem it was called on, then adds that to the end of the parent container(list) element on the DOM.
   attachToCart() {
     CartItem.cartItemsList.appendChild(this.renderCartItem());
   }
-
   // Sets the actual innerHTML of THIS cartItem
   renderCartItem() {
     this.element.innerHTML = `<div class="d-flex flex-row px-2 border-bottom">
@@ -49,8 +85,9 @@ class CartItem {
 
           <div class="flex-column flex-fill ps-1">
             <div class="row mt-3 fs-5 lh-1">
-              <span class="cartItemTitle" id="cartItem-${this.id}Title">${this.name}</span>
+              <span class="cartItemTitle" id="cartItem-${this.id}Title">${this.item_name}</span>
             </div>
+
             <div class="row">
               <span  class="cartItemStars" id="cartItem-${this.id}-Stars"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f5a31b" class="bi bi-star" viewBox="0 0 16 16" stroke="#d39a63" stroke-width="0.25"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f5a31b" class="bi bi-star" viewBox="0 0 16 16" stroke="#d39a63" stroke-width="0.25"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f5a31b" class="bi bi-star" viewBox="0 0 16 16" stroke="#d39a63" stroke-width="0.25"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f5a31b" class="bi bi-star" viewBox="0 0 16 16" stroke="#d39a63" stroke-width="0.25"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f5a31b" class="bi bi-star" viewBox="0 0 16 16" stroke="#d39a63" stroke-width="0.25"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f5a31b" class="bi bi-star" viewBox="0 0 16 16" stroke="#d39a63" stroke-width="0.25">
 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
@@ -69,7 +106,12 @@ class CartItem {
               <span class="cartItemGiftCheck"><small><input class="form-check-input lh-1 fs-6" type="checkbox" value="" id="cartItem-${this.id}-GiftCheck"> This is a gift</small></span>
             </div>
             <div class="row">
-              <span class="cartItemColor"><small><span class="fw-bolder " id="cartItem-${this.id}-Color">Color:</span> ${this.color}</small></span>
+              <span class="cartItemColor">
+                <small>
+                  <span class="fw-bolder" id="cartItem-${this.id}-Color">Style:</span> 
+                  <span class="cartItemStyle" id="cartItem-${this.id}Style">${this.style_name}</span>
+                </small>
+              </span>
             </div>
             <div class="row" id="cartItem-${this.id}-Manufacturer">
               <span class="cartItemManufacturer"><small><span class="fw-bolder" id="cartItem-${this.id}">Manufacturer:</span> ${this.manufacturer}</small></span>
@@ -120,54 +162,7 @@ class CartItem {
     return this.element;
   }
 
-  updateCartItem(quantity) {
-    this.quantity = quantity;
-    Cart.currentCart().orderInfo.styles.find(e => e.id = this.id).quantity = quantity;
-    this.renderCartItem()
-  }
 
-  handleCartItemClick = (e) => {
-    e.preventDefault();
-    // Provides a path and action for all of the cartItem elements on the page. This is the main hub for all CRUD Action Initiation.
-    let clickedOn = e.target.className;
-
-    switch (clickedOn) {
-      case "cartItemTitle":
-      case "cartItemPrice":
-      case "cartItemEligible":
-      case "cartItemStars":
-      case "cartItemColor":
-      case "cartItemManufacturer":
-        //should flash a notice on screen that careful, youre headed out of the one page app!
-        alert(
-          "Watch Out! You're about to head on your way out of my single page app, why don't ya turn on around!"
-        );
-        break;
-
-      case "dropdown-item":
-        //*updates the cartItem on the front end and DOM
-        this.updateCartItem(e.target.innerText)
-
-        //*updates the Cart FrontEnd and BackEnd wise, calls CartAPI for the update.
-        Cart.currentCart().updateCartInfo()
-        // ??Then similarly do the same with the cart, but in this case the loop also sends the persist update to the backend
-
-        debugger
-        break;
-
-      case "deleteItemFromCartLinkAndAddToList":
-        debugger;
-        break;
-      
-      case "deleteItemFromCartLink":  
-        debugger;
-        break;
-      default:
-        // ?acts as a catch for everything that got missed, and allows bstrap Popper JS to still handle its events correctly.
-        console.log("null event");
-        break;
-    }
-  };
 
 
 }
