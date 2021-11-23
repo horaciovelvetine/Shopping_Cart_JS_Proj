6ab9c8b6f3a4298748ed8bcdcaba0d24240751b0
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-22 17:25:32 -0700

    update to remove excess data, fix update

--- src/cart.js
+++ src/cart.js
@@ -3,20 +3,19 @@ class Cart {
   static cartBarn = [];
   static belowCartInfoDisplay = document.getElementById("belowCartInfoDisplay");
   static sidebarCartInfoDisplay = document.getElementById("sidebarCartInfoDisplay");
-  
-  // ?Easily retrieve the js instance of Cart OBj
+
+  //* Easily retrieve the js instance of Cart OBj
   static currentCart = () => {
     return Cart.cartBarn["0"];
   };
 
-  constructor({ id, number_of_items, sub_total, to_order }) {
+  constructor({ id, number_of_items, sub_total }) {
     // Begin setting values for a Cart OBj's attributes using the recieved data from the API
     //data.attributes
     this.id = id;
     this.numberOfItems = `${number_of_items}`;
     this.subTotal = `${sub_total}`;
     // Include nested attributes for objects which are passed thru to create the order...
-    this.orderInfo = to_order;
     // Annonymous functionise these attributes to return the class' to effectively maintain updated objects for each
     this.cartItems = () => {
       return CartItem.all;
@@ -40,30 +39,25 @@ class Cart {
     this.attatchCartInfoToDom();
   }
 
-  
-  // ?This sets the cart attributes to 0 and then calculates the new totals.
+  //* This sets the cart attributes to 0 and then calculates the new totals.
   updateCartInfo() {
     this.subTotal = 0;
     this.numberOfItems = 0;
-    
-    
-    this.cartItems(this).forEach( item => {
+
+    this.cartItems(this).forEach(item => {
       let cart = this
       let price = parseFloat(item.price)
       let quantity = parseInt(item.quantity)
       cart.subTotal += (price * quantity)
       cart.numberOfItems += quantity
-      
+
     })
     this.subTotal = this.subTotal.toFixed(2)
     this.attatchCartInfoToDom();
-    
-    
+
     cartApi.sendPatch(this)
-    debugger
   }
-  
-  
+
   //* Calls both of the below functions to attatch info in multiple locations
   attatchCartInfoToDom() {
     this.attatchCartInfoToDomEndCart();
@@ -73,13 +67,13 @@ class Cart {
     // Uses the variable value to set the innerHTML to avoid attempting to place this.element more than once on the DOM. OO JS...
     Cart.sidebarCartInfoDisplay.innerHTML = this.element.innerHTML;
   }
-
   attatchCartInfoToDomEndCart() {
     Cart.belowCartInfoDisplay.appendChild(this.renderCartInfo());
   }
-
   renderCartInfo() {
     this.element.innerHTML = `Subtotal (${this.numberOfItems} items): <span class="fw-bolder"> $${this.subTotal}</span>`;
     return this.element;
   }
+
+
 }
