cc1d36c2a46134739a8b3731fd308f14b3ea0e9a
JimmyJamFlimFlam <78946408+horaciovelvetine@users.noreply.github.com>
2021-11-22 17:25:56 -0700

    creates alert element, working update patch

--- src/cartApi.js
+++ src/cartApi.js
@@ -1,9 +1,18 @@
 class CartApi {
 
+  // static notificationsBucket = docuement.getElementById("notificationsBucket");
+  static notificationBucket = document.getElementById("notificationBucket");
 
   constructor(port) {
     this.demoUrl = `${port}/show_cart`
     this.baseUrl = `${port}/carts`
+
+    this.element = document.createElement('li')
+    this.element.innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+    Cart Updated <strong>Successfully</strong><button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>`
+    this.element.dataset.id = 'notification'
+    this.element.id = 'notification'
   }
 
   getCartDetails() {
@@ -38,25 +47,33 @@ class CartApi {
   }
 
   sendPatch(cart) {
-    console.log("gotme!")
-    
+    let { id, numberOfItems, subTotal } = cart
+    let cartItems = cart.cartItems()
+    const payload = {
+      id,
+      numberOfItems,
+      subTotal,
+      cartItems
+    }
 
-    debugger
+    const configOBj = {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(payload)
+    }
 
-    // const configObj = {
-    //   method: 'PATCH',
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Accept: "application/json"
-    //   },
-    //   body: JSON.stringify(payload)
-    // }
+    fetch(`${this.baseUrl}/${cart.id}`, configOBj).then((res) => res.json()).then(json => {
+      cartApi.attatchResponseToDom();
+      return console.log("Update success", json)
+    })
 
-    // fetch(`${this.baseUrl}/${payload.id}`, configObj).then(response => response.json()).then(json => {
-    //   console.log(json)
-    //   debugger
-    // })
+  }
 
+  attatchResponseToDom() {
+    notificationBucket.appendChild(this.element);
   }
 }
 
